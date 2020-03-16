208b818d88735fb3eb5472a1bb8b2471b9dc1990
Evgeny Chaptykov <egchaptykov@mail.ru>
2020-03-16 11:37:41 +0700

    Урок 4.1

--- lesson-4-ex-7.py
+++ lesson-4-ex-7.py
@@ -10,16 +10,17 @@ def generator_range():
 
 def generator_count():
     for i in count(1):
-        if i < 16:
-            yield factorial(i)
-        else:
-            print('Работа завершена')
-            return
+        yield factorial(i)
 
 fibo_gen = generator_range()
 for el in fibo_gen:
     print(el)
 
 fibo_gen = generator_count()
+i = 1
 for el in fibo_gen:
+    if i > 15:
+        print("Работа завершена")
+        break
+    i += 1
     print(el)
